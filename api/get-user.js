46e60405b7975814abdedda53690e6bb8779ed48
Goeddder <115229790+Goeddder@users.noreply.github.com>
2026-02-24 19:09:23 +0200

    Create api/get-user.js

---
+++ api/get-user.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+export default async function handler(req, res) {
+    const { username } = req.query;
+    // Твой токен бота
+    const BOT_TOKEN = '8617323759:AAGtVEQ16R8lHU9x8jZ4I5MDiEvc6d1HZGE';
+
+    if (!username) return res.status(400).json({ ok: false });
+
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/getChat?chat_id=@${username.replace('@', '')}`);
+        const data = await response.json();
+
+        if (data.ok) {
+            res.status(200).json({
+                ok: true,
+                name: data.result.first_name + (data.result.last_name ? ' ' + data.result.last_name : ''),
+                photo: `https://t.me/i/userpic/320/${username.replace('@', '')}.jpg`
+            });
+        } else {
+            res.status(404).json({ ok: false });
+        }
+    } catch (e) {
+        res.status(500).json({ ok: false });
+    }
+}
