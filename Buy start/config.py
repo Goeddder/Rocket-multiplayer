71d6c5737135ef30ce2ca4c651d815134096e1e6
Goeddder <115229790+Goeddder@users.noreply.github.com>
2026-02-25 22:58:01 +0200

    Update config.py

--- Buy start/config.py
+++ Buy start/config.py
@@ -1,19 +1,28 @@
+# Buy_start/config.py
 
-API_TON = ""
+API_TON = "AGKI2KZG34NPRAYAAAAGFX6R5LEBGZXNOAEFMWAOP6S324YL64IOXTMYHAQ3UY5CWTQRILY"
 
+# Твои 24 слова (проверь порядок)
 MNEMONIC = [
-
+    "penalty", "need", "easy", "fortune", "slogan", "planet",
+    "super", "alpha", "silent", "shoulder", "idle", "absent",
+    "obtain", "mouse", "blouse", "ill", "analyst", "shiver",
+    "regret", "bird", "favorite", "leisure", "alpha", "elder"
 ]
 
+# Куки из твоего сообщения
 DATA = {
-    'stel_ssid': '',
-    'stel_dt': '',
-    'stel_ton_token': '-',
-    'stel_token': '',
+    'stel_ssid': '0a791baaefc2da4f7e_17021482322052895780',
+    'stel_dt': '-120',
+    'stel_ton_token': 'TXCMUcx3By2LsEHL5sPhz0c8blblgPTiLA5Pz6g88_PoL09Dq4DWjP35rjnElv79TBrMCxyIiTSnlay9sQoqeZOXBg3FxfYjLZ8At16S7lrNQJArm8JVCODO_nZXSC0bJeFXEzLI00F0rkS19dv0Ric0xyudXCz9Sq9JWs_aCS9nueKCrQSw9gR46GAbShRc3_ucCFj-',
+    'stel_token': 'e9f5c227d3bdf60f806ac6634b6ba4bde9f5c23ce9f5c9fd14f43059def026b1eb6b6',
 }
 
-FRAGMENT_HASH = ''
-FRAGMENT_PUBLICKEY = ''
-FRAGMENT_WALLETS = ''
+FRAGMENT_HASH = '747c09519b10e540aa'
 
-FRAGMENT_ADDRES = ''
+# Данные из твоего последнего скриншота Payload
+FRAGMENT_ADDRES = 'a692ee022f931499b2f37c698bbd688be0acaa4a89d20d103869109' # параметр 'to'
+FRAGMENT_PUBLICKEY = '61ac78017154af221fe8916d2190d1fb4a76748e847f211cd341da8ad81b3b5b' # твой ключ
+
+# Твой кошелек для приема ручных платежей TON
+MY_WALLET_ADDR = 'UQATIzX89EOOTqfsr8KE64MXvVSa4CGybjwmkPv3-yHbI3ZE'
